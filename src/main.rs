6b92115ed27d17d7239f71fe3f8ab07489576239
Emanuel Vollmer <emanuel.vollmer@mayflower.de>
2021-08-20 12:16:01 +0000

    Remove game

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::time::Duration;
 
-use game::Game;
 use termion;
 use termion::raw::IntoRawMode;
 use termion::terminal_size;
@@ -8,7 +7,6 @@ use termion::terminal_size;
 use std::io::{Read, Write};
 
 mod direction;
-mod game;
 mod point;
 
 fn main() {
@@ -29,34 +27,10 @@ fn main() {
     .ok();
     let (width, height) = terminal_size().expect("Can't get terminal size, exiting....");
 
-    let mut gamestate = Game::new(height.into(), width.into());
-
     loop {
         write!(&mut stdout, "{}", termion::clear::All,).ok();
 
         let key = stdin.next();
-
-        match key {
-            Some(Ok(b'c')) => {
-                write!(
-                    &mut stdout,
-                    "{}{}{}",
-                    termion::clear::All,
-                    termion::cursor::Goto(1, 1),
-                    termion::cursor::Show
-                )
-                .ok();
-                return;
-            }
-            Some(Ok(x)) => {
-                gamestate.update(x.into());
-            }
-            _ => {
-                gamestate.update(' ');
-            }
-        }
-
-        gamestate.draw(&mut stdout);
 
         stdout.flush().unwrap();
         std::thread::sleep(Duration::from_millis(333))
